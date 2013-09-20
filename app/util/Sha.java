a98deb8a727a6a60d546de8d71aae4cddf67030b
Web Dev <cdc@ubuntu.(none)>
2013-09-19 20:57:22 -0500

    Fix

---
+++ app/util/Sha.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.security.*;
+public class Sha {
+	public static String pwdhash(String pwd) throws NoSuchAlgorithmException {
+		for (int i=0; i<100; i++) {
+			pwd = hash512(pwd);
+		}
+		return pwd;
+	}
+	
+public static String hash512(String data) throws NoSuchAlgorithmException {
+MessageDigest md = MessageDigest.getInstance("SHA-512");
+md.update(data.getBytes());
+return bytesToHex(md.digest());
+}
+public static String bytesToHex(byte[] bytes) {
+StringBuffer result = new StringBuffer();
+for (byte byt : bytes) result.append(Integer.toString((byt & 0xff) + 0x100, 16).substring(1));
+return result.toString();
+}
+}
